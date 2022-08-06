37c48f795e471864873d6be7f2450150aa8caadf
Aishwarya2007 <110040964+Aishwarya2007@users.noreply.github.com>
2022-08-06 00:56:26 -0700

    updated

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Docker";
+			return "Vanakkam da Mapla karaikudi la irunthu StonyMafia";
 		}else{
 			return "Hello " + name;
 		}
